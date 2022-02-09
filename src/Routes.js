57528764e4aede3cd3a263faa90bdccc65528e15
William Nollert <will.nollert@gmail.com>
2022-02-08 21:24:33 -0600

    added notFound catchAll route

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/home";
+import NotFound from "./containers/NotFound";
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/">
         <Home />
+      </Route>
+      <Route>
+        <NotFound />
       </Route>
     </Switch>
   );

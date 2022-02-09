e15d397116681457de885499aeb5c43814133a99
William Nollert <will.nollert@gmail.com>
2022-02-08 21:20:44 -0600

    set up NavBar with reactRouter + Bootsraps

---
+++ src/containers/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Fitness Tracker</h1>
+        <p className="text-muted">Commit to your 2022</p>
+      </div>
+    </div>
+  );
+}
